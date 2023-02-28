c98bd75ecabbf3d6dc830d8b0b38438cf08eca61
SaludaDev <sistemas@saluda.mx>
2023-02-28 15:57:25 -0600

    Update ControlPOS.php

--- App/Secure/ControlPOS.php
+++ App/Secure/ControlPOS.php
@@ -2,40 +2,40 @@
 ini_set('error_reporting', E_ALL);
 if($_SESSION["SuperAdmin"])	//Condicion admin
 {
-	header("location:https://controlfarmacia.com/AdminPOS");	
+	header("location:https://saludaclinicas.com/AdminPOS");	
 
 }
 if($_SESSION["VentasPos"])	//Condicion personal
 {
-	header("location: https://controlfarmacia.com/POS2"); 
+	header("location: https://saludaclinicas.com/POS2"); 
 }
 
 
 if($_SESSION["LogisticaPOS"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/POSLogistica"); 
+	header("location: https://saludaclinicas.com/POSLogistica"); 
 }
 
 if($_SESSION["ResponsableCedis"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/CEDIS"); 
+	header("location: https://saludaclinicas.com/CEDIS"); 
 }
 
 if($_SESSION["ResponsableInventarios"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/Inventarios"); 
+	header("location: https://saludaclinicas.com/Inventarios"); 
 }
 
 if($_SESSION["ResponsableDeFarmacias"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/ResponsableDeFarmacias");
+	{	header("location: https://saludaclinicas.com/ResponsableDeFarmacias");
 	}
 	if($_SESSION["CoordinadorDental"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/JefeDental");
+	{	header("location: https://saludaclinicas.com/JefeDental");
 	}
 	if($_SESSION["Supervisor"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/CEDISMOVIL");
+	{	header("location: https://saludaclinicas.com/CEDISMOVIL");
 	}
 ?>
