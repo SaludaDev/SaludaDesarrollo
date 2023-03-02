bb7b39f9683afeda13da7fbc53837df24c46393f
SaludaDev <sistemas@saluda.mx>
2023-03-02 15:07:49 -0600

    Update ControladorAgenda.php

--- App/Secure/ControladorAgenda.php
+++ App/Secure/ControladorAgenda.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+$_SESSION['nombre'] = 'Goku';
+$_SESSION['raza'] = 'Saiyan';
+
+echo $_SESSION['nombre'];
 include ("Scripts/PersonalAgenda.php");
 if($_SESSION["AdminAgenda"])	//Condicion admin
 {
