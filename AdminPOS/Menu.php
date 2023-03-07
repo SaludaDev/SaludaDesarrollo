bf70473af88ca6faf69fb7a6e2c42220a7b3133d
SaludaDev <sistemas@saluda.mx>
2023-03-07 12:25:53 -0600

    Update Menu.php

--- AdminPOS/Menu.php
+++ AdminPOS/Menu.php
@@ -120,7 +120,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../Perfiles/<?echo $row['file_name']?>" class="img-circle elevation-2" alt="User Image">
+          <img src="https://saludaclinicas.com/Perfiles/<?php echo $row['file_name']?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a  id="DatosGenerales" class="d-block"><?php echo $row['Nombre_Apellidos']?></a>
