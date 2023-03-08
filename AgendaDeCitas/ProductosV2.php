042e03d654f51fa4e310889c57df5efc4010c713
JuanSaluda <soporte1@saluda.mx>
2023-03-08 12:17:09 -0600

    Cambios

--- AgendaDeCitas/ProductosV2.php
+++ AgendaDeCitas/ProductosV2.php
@@ -6,30 +6,30 @@ include "Consultas/AnalisisIndex.php";
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Base general de productos de <?echo $row['ID_H_O_D']?> </title>
+  <title>Base general de productos de <? echo $row['ID_H_O_D'] ?> </title>
 
-<?include "Header.php"?>
- <style>
-        .error {
-  color: red;
-  margin-left: 5px; 
-  
-}
+  <?php include "Header.php" ?>
+  <style>
+    .error {
+      color: red;
+      margin-left: 5px;
 
-    </style>
+    }
+  </style>
 </head>
-<?include_once ("Menu.php")?>
+<?php include_once("Menu.php") ?>
 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
   <li class="nav-item">
     <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Base de datos de productos</a>
   </li>
-  
- 
+
+
   <!-- <li class="nav-item">
     <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#PorCaducar" role="tab" aria-controls="pills-contact" aria-selected="false">Productos prontos a caducar</a>
   </li>
@@ -39,26 +39,26 @@ include "Consultas/AnalisisIndex.php";
 </ul>
 
 <div class="tab-content" id="pills-tabContent">
-<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-<div class="card text-center">
-  <div class="card-header" style="background-color:#2b73bb !important;color: white;">
-    Listado de productos de <?echo $row['ID_H_O_D']?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
+  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+    <div class="card text-center">
+      <div class="card-header" style="background-color:#2b73bb !important;color: white;">
+        Listado de productos de <? echo $row['ID_H_O_D'] ?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+
+    <div id="tablaProductos"></div>
+
   </div>
-  
-  <div >
-
-</div>
-</div>
-    
-<div id="tablaProductos"></div>
-
-</div>
 
 
- <!--  <div class="tab-pane fade" id="PorCaducar" role="tabpanel" aria-labelledby="pills-profile-tab">
+  <!--  <div class="tab-pane fade" id="PorCaducar" role="tabpanel" aria-labelledby="pills-profile-tab">
   <div class="card text-center">
   <div class="card-header" style="background-color:#2b73bb !important;color: white;">
-  Productos por caducar de <?echo $row['ID_H_O_D']?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
+  Productos por caducar de <? echo $row['ID_H_O_D'] ?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
   </div>
   
   <div >
@@ -71,29 +71,28 @@ include "Consultas/AnalisisIndex.php";
 
 </div>
 
-    
-</div></div>
+
+</div>
+</div>
 
 
 
 
 
-     
-  
-  <!-- /.content-wrapper -->
 
-  <!-- Control Sidebar -->
- 
-  <!-- Main Footer -->
+
+<!-- /.content-wrapper -->
+
+<!-- Control Sidebar -->
+
+<!-- Main Footer -->
 <?php
-  include ("Modales/AltaProductos.php");
-
-  include ("Modales/Vacios.php");
-  include ("Modales/Error.php");
-  include ("Modales/Exito.php");
-  include ("Modales/ExitoActualiza.php");
-
-  include ("footer.php")?>
+include("Modales/AltaProductos.php");
+include("Modales/Vacios.php");
+include("Modales/Error.php");
+include("Modales/Exito.php");
+include("Modales/ExitoActualiza.php");
+include("footer.php") ?>
 
 <!-- ./wrapper -->
 
@@ -104,12 +103,12 @@ include "Consultas/AnalisisIndex.php";
 
 
 
-<script src="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>  
-    <script src="datatables/JSZip-2.5.0/jszip.min.js"></script>    
-    <script src="datatables/pdfmake-0.1.36/pdfmake.min.js"></script>    
-    <script src="datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
-    <script src="datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+<script src="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
+<script src="datatables/JSZip-2.5.0/jszip.min.js"></script>
+<script src="datatables/pdfmake-0.1.36/pdfmake.min.js"></script>
+<script src="datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
+<script src="datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
 
 
 <!-- Bootstrap -->
@@ -124,20 +123,21 @@ include "Consultas/AnalisisIndex.php";
 
 <!-- PAGE PLUGINS -->
 <script>
+  $('document').ready(function($) {
+    $('#Precarga').modal('toggle');
+    setTimeout(function() {
+      $('#Precarga').modal('hide')
+    }, 5000); // abrir
 
-$('document').ready(function($){
-$('#Precarga').modal('toggle'); 
-setTimeout(function(){ 
-    $('#Precarga').modal('hide') 
-}, 5000); // abrir
-
-});	   
+  });
 </script>
 </body>
-</html>
-<?
 
-function fechaCastellano ($fecha) {
+</html>
+<?php
+
+function fechaCastellano($fecha)
+{
   $fecha = substr($fecha, 0, 10);
   $numeroDia = date('d', strtotime($fecha));
   $dia = date('l', strtotime($fecha));
@@ -146,9 +146,9 @@ function fechaCastellano ($fecha) {
   $dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
   $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
   $nombredia = str_replace($dias_EN, $dias_ES, $dia);
-$meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+  $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
   $meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
   $nombreMes = str_replace($meses_EN, $meses_ES, $mes);
-  return $nombredia." ".$numeroDia." de ".$nombreMes." de ".$anio;
+  return $nombredia . " " . $numeroDia . " de " . $nombreMes . " de " . $anio;
 }
 ?>
