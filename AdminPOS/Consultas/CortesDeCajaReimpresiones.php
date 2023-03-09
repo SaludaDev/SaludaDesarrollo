40893aec78e1eb0dd70aa70d49d8e48464836cce
SaludaDev <sistemas@saluda.mx>
2023-03-09 14:21:17 -0600

    f

--- AdminPOS/Consultas/CortesDeCajaReimpresiones.php
+++ AdminPOS/Consultas/CortesDeCajaReimpresiones.php
@@ -25,7 +25,7 @@ include "Sesion.php";
 $user_id=null;
 $sql1="SELECT Cajas_POS.ID_Caja,Cajas_POS.Cantidad_Fondo,Cajas_POS.Empleado,Cajas_POS.Sucursal,Cajas_POS.Estatus,Cajas_POS.CodigoEstatus,Cajas_POS.Turno,Cajas_POS.Asignacion,
 Cajas_POS.Fecha_Apertura,Cajas_POS.Valor_Total_Caja,Cajas_POS.ID_H_O_D, SucursalesCorre.ID_SucursalC, SucursalesCorre.Nombre_Sucursal 
-FROM Cajas_POS,SucursalesCorre where Cajas_POS.Sucursal = SucursalesCorre.ID_SucursalC AND Cajas_POS.Estatus='Cerrada'  AND Cajas_POS.ID_H_O_D='".$row['ID_H_O_D']."'  AND Cajas_POS.Sucursal='".$row['Fk_Sucursal']."' ";
+FROM Cajas_POS,SucursalesCorre where Cajas_POS.Sucursal = SucursalesCorre.ID_SucursalC AND Cajas_POS.Estatus='Cerrada'  AND Cajas_POS.ID_H_O_D='.$row['ID_H_O_D'].'  AND Cajas_POS.Sucursal='".$row['Fk_Sucursal']."' ";
 $query = $conn->query($sql1);
 ?>
 
