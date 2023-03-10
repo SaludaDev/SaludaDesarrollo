23f0263a90797ba56879c3a65592fcb09897086e
SaludaDev <sistemas@saluda.mx>
2023-03-10 09:20:30 -0600

    VentasLibres

--- AdminPOS/Consultas/VentasDelDiaLibres.php
+++ AdminPOS/Consultas/VentasDelDiaLibres.php
@@ -1,4 +1,3 @@
-
 <script>
 
 tabla = $('#Productos').DataTable({
@@ -8,7 +7,7 @@ tabla = $('#Productos').DataTable({
  "stateSave":true,
  "bAutoWidth": true,
  "order": [[ 0, "desc" ]],
- "sAjaxSource": "https://controlfarmacia.com/AdminPOS/Consultas/ArrayDesgloseVentasLibres.php",
+ "sAjaxSource": "https://saludaclinicas.com/AdminPOS/Consultas/ArrayDesgloseVentasLibres.php",
  "aoColumns": [
        { mData: 'Cod_Barra' },
        { mData: 'Nombre_Prod' },
@@ -106,7 +105,7 @@ tabla = $('#Productos').DataTable({
 
 </div>
 </div>
-<?
+<?php 
 
 function fechaCastellano ($fecha) {
   $fecha = substr($fecha, 0, 10);
@@ -124,94 +123,3 @@ $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"
 }
 ?>
 
-<!-- <script>
-  $(".btn-edit").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/ReasignaProducto.php","id="+id,function(data){
-        alert($(this).data("id"));
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Asignacion de productos en otras sucursales");
-       
-        $("#DiProductosG").removeClass("modal-dialog  modal-xl modal-notify modal-info");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-success");
-    });
-    $('#editModalProductosG').modal('show'); 
-});
-$(".btn-VerDistribucion").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/DistribucionesProductos.php","id="+id,function(data){
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Distribucion de productos");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-info");
-    });
-    $('#editModalProductosG').modal('show');
-});
-$(".btn-editProd").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/EditaProductosStockGeneral.php","id="+id,function(data){
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Editar datos");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-info");
-    });
-    $('#editModalProductosG').modal('show');
-});
-$(".btn-History").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/HistorialProductos.php","id="+id,function(data){
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Actualizaciones y movimientos");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-info");
-    });
-    $('#editModalProductosG').modal('show');
-});
-
-
-$(".btn-Delete").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/DeleteProductos.php","id="+id,function(data){
-        alert($(this).data("id"));
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Eliminar producto");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").removeClass("modal-dialog  modal-xl modal-notify modal-info");
-        $("#DiProductosG").addClass("modal-dialog modal-sm modal-notify modal-danger");
-    });
-    $('#editModalProductosG').modal('show');
-});
-
-
-</script>
-
-
-  <div class="modal fade" id="editModalProductosG" tabindex="-1" role="dialog" style="overflow-y: scroll;" aria-labelledby="editModalLabel" aria-hidden="true">
-  <div id="DiProductosG"class="modal-dialog modal-lg modal-notify modal-info">
-      <div class="modal-content">
-      <div class="modal-header">
-         <p class="heading lead" id="TituloProductosG"></p>
-
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-           <span aria-hidden="true" class="white-text">&times;</span>
-         </button>
-       </div>
-       
-	        <div class="modal-body">
-          <div class="text-center">
-        <div id="form-editProductosG"></div>
-        
-        </div>
-
-      </div>
-    </div>
-  </div> -->
