06bf4a970ce9e7dcb77c475d342f8630a19fb7c8
SaludaDev <sistemas@saluda.mx>
2023-03-10 09:14:46 -0600

    Update VentasEnGeneral.php

--- AdminPOS/VentasEnGeneral.php
+++ AdminPOS/VentasEnGeneral.php
@@ -11,7 +11,7 @@ include "Consultas/AnalisisIndex.php";
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Ventas realizadas por  <?echo $row['ID_H_O_D']?> <?echo $row['Nombre_Sucursal']?> </title>
+  <title>Ventas realizadas por  <?php echo $row['ID_H_O_D']?> <?php echo $row['Nombre_Sucursal']?> </title>
 
 <?php include "Header.php"?>
  <style>
