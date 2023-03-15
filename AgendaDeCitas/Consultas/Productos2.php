33faa1efd93b077b725d83da5c09d918bb227a66
JuanSaluda <soporte1@saluda.mx>
2023-03-15 15:41:31 -0600

    Cambios Lista de productos

--- AgendaDeCitas/Consultas/Productos2.php
+++ AgendaDeCitas/Consultas/Productos2.php
@@ -1,87 +1,97 @@
-
 <script>
+    tabla = $('#ProductosLista').DataTable({
 
-tabla = $('#Productos').DataTable({
+        "bProcessing": true,
+        "ordering": true,
+        "stateSave": true,
+        "bAutoWidth": false,
+        "order": [
+            [0, "desc"]
+        ],
+        "sAjaxSource": "https://saludaclinicas.com/AgendaDeCitas/Consultas/ArrayProductos.php",
+        "aoColumns": [
 
- "bProcessing": true,
- "ordering": true,
- "stateSave":true,
- "bAutoWidth": false,
- "order": [[ 0, "desc" ]],
- "sAjaxSource": "https://saludaclinicas.com/AgendaDeCitas/Consultas/ArrayProductos.php",
- "aoColumns": [
-   
-       { mData: 'Cod_Barra' },
-       { mData: 'Nombre_Prod' },
-      
-       
-       { mData: 'Precio_Venta' },
-       { mData: 'Nom_Serv' },
-    
-    //    { mData: 'Vendido' },
-    //    { mData: 'Saldo' },
-      
-  
-      ],
-     
-    
-      "lengthMenu": [[20,50,150,250,500, -1], [20,50,150,250,500, "Todos"]],  
-  
-      language: {
-            "lengthMenu": "Mostrar _MENU_ registros",
-                "zeroRecords": "No se encontraron resultados",
-                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sSearch": "Buscar:",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast":"Último",
-                    "sNext":"Siguiente",
-                    "sPrevious": "Anterior"
-			     },
-			     "sProcessing":"Procesando...",
+            {
+                mData: 'Cod_Barra'
             },
-          
+            {
+                mData: 'Nombre_Prod'
+            },
+
+
+            {
+                mData: 'Precio_Venta'
+            },
+            {
+                mData: 'Nom_Serv'
+            },
+
+            //    { mData: 'Vendido' },
+            //    { mData: 'Saldo' },
+
+
+        ],
+
+
+        "lengthMenu": [
+            [20, 50, 150, 250, 500, -1],
+            [20, 50, 150, 250, 500, "Todos"]
+        ],
+
+        language: {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Procesando...",
+        },
+
         //para usar los botones   
         responsive: "true",
-        dom: "B<'#colvis row'><'row'><'row'<'col-md-6'l><'col-md-6'f>r>t<'bottom'ip><'clear'>'",
-    //    // buttons:[ 
-	// 		{
-	// 			extend:    'excelHtml5',
-	// 			text:      'Descargar excel  <i Descargar excel class="fas fa-file-excel"></i> ',
-	// 			titleAttr: 'Descargar excel',
-    //             autoFilter: true,
-    //     title: 'Base general de productos ',
-	// 			className: 'btn btn-success'
-	// 		},
-    //     //],
-       
-       
-   
-	   
-        	        
-    });     
+        dom: "<'#colvis row'><'row'><'row'<'col-md-6'l><'col-md-6'f>r>t<'bottom'ip><'clear'>'",
+        //    // buttons:[ 
+        // 		{
+        // 			extend:    'excelHtml5',
+        // 			text:      'Descargar excel  <i Descargar excel class="fas fa-file-excel"></i> ',
+        // 			titleAttr: 'Descargar excel',
+        //             autoFilter: true,
+        //     title: 'Base general de productos ',
+        // 			className: 'btn btn-success'
+        // 		},
+        //     //],
 
+
+
+
+
+    });
 </script>
 <div class="text-center">
-	<div class="table-responsive">
-	<table  id="Productos" class="table table-hover">
-<thead>
+    <div class="table-responsive">
+        <table id="ProductosLista" class="table table-hover">
+            <thead>
 
-<th>Cod Barra</th>
-    <th>Nombre</th>
-   
-    <th>Precio de venta </th>
-    <th>Servicio </th>
-  
-   
-	
+                <th>Cod Barra</th>
+                <th>Nombre</th>
+
+                <th>Precio de venta </th>
+                <th>Servicio </th>
 
 
-</thead>
 
-</div>
+
+
+            </thead>
+
+    </div>
 </div>
 
 
