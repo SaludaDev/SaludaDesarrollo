d2c3187ebb7c1e044aa141f583e6c40cdbb77182
SaludaDev <sistemas@saluda.mx>
2023-03-23 16:38:52 -0600

    Update Consultas.php

--- POS2/Consultas/Consultas.php
+++ POS2/Consultas/Consultas.php
@@ -4,7 +4,7 @@ session_start();
 if(!isset($_SESSION['VentasPos'])){
 	header("Location: Expiro.php");
 }
-include_once("../db_connect.php");
+include_once("db_connect.php");
 $sql ="SELECT PersonalPOS.Pos_ID,PersonalPOS.Nombre_Apellidos,PersonalPOS.file_name,PersonalPOS.Fk_Usuario,PersonalPOS.Fk_Sucursal,PersonalPOS.ID_H_O_D,PersonalPOS.Estatus,
 Roles_Puestos.ID_rol,Roles_Puestos.Nombre_rol, SucursalesCorre.ID_SucursalC,SucursalesCorre.Nombre_Sucursal,SucursalesCorre.Cuenta_Clip,SucursalesCorre.Clave_Clip from PersonalPOS,Roles_Puestos,SucursalesCorre 
 where PersonalPOS.Fk_Usuario = Roles_Puestos.ID_rol and PersonalPOS.Fk_Sucursal = SucursalesCorre.ID_SucursalC and PersonalPOS.Pos_ID='".$_SESSION['VentasPos']."'"; 
