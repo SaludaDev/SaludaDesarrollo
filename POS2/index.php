085a7d867a6ea5808e842cc1a9dbd0fe2652117f
SaludaDev <sistemas@saluda.mx>
2023-03-24 11:31:04 -0600

    Update index.php

--- POS2/index.php
+++ POS2/index.php
@@ -13,10 +13,10 @@ if(!isset($_COOKIE["IngresoVentas"]))
 }
 include "Consultas/Consultas.php";
 include "Consultas/Sesion.php";
-include "Consultas/NotificacionesApp.php";
- include "Consultas/Conexion_selects.php";
- include "Consultas/ConeSelectDinamico.php";
-include "Consultas/ConsultaFondoCaja.php";
+// include "Consultas/NotificacionesApp.php";
+//  include "Consultas/Conexion_selects.php";
+//  include "Consultas/ConeSelectDinamico.php";
+// include "Consultas/ConsultaFondoCaja.php";
 
 ?>
 <!DOCTYPE html>
