3ac74fc261f9505f011c41cc79a6adaef2aef924
SaludaDev <sistemas@saluda.mx>
2023-03-28 09:13:03 -0600

    Update AbreCajaIndex.php

--- POS2/Modales/AbreCajaIndex.php
+++ POS2/Modales/AbreCajaIndex.php
@@ -128,7 +128,7 @@ while ($r=$query->fetch_object()){
         <?php
           $query = $conn -> query ("SELECT Medico_ID,Nombre_Apellidos,ID_H_O_D,Fk_Sucursal,Estatus FROM Personal_Medico WHERE Estatus='Vigente' AND ID_H_O_D='".$row['ID_H_O_D']."' AND Fk_Sucursal='".$row['Fk_Sucursal']."' ");
           while ($valores = mysqli_fetch_array($query)) {
-            echo '<option value="'.$valores["Nombre_Apellidos"].'">'.$valores["Nombre_Apellidos"].'</option>';
+            echo '<option value="'.$valores[Nombre_Apellidos].'">'.$valores[Nombre_Apellidos].'</option>';
           }
         ?>  </select>  
   </div>
