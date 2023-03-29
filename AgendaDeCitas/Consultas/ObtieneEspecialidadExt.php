8f88b027f76faa35f7fce8379aa64294053f86a4
SaludaDev <sistemas@saluda.mx>
2023-03-29 15:24:12 -0600

    Update ObtieneEspecialidadExt.php

--- AgendaDeCitas/Consultas/ObtieneEspecialidadExt.php
+++ AgendaDeCitas/Consultas/ObtieneEspecialidadExt.php
@@ -1,7 +1,7 @@
 <?php
 	include("ConeSelectDinamico.php");
 	$sucursal=intval($_REQUEST['sucursal']);
-	$medicos = $conn->prepare("SELECT ID_Especialidad,Nombre_Especialidad,Fk_Sucursal FROM Especialidades_Express WHERE Fk_Sucursal = '$sucursal'") or die(mysqli_error($conn));
+	$medicos = $conn->prepare("SELECT ID_Especialidad,Nombre_Especialidad,Fk_Sucursal FROM Especialidades_Express WHERE Fk_Sucursal = '$sucursal'") ;
 		echo '<option value = "">Selecciona un especialidad  </option>';
 	if($medicos->execute()){
 		$a_result = $medicos->get_result();
