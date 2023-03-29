cf6746bbec601bfe3acf4c288e339db63e3d1f9b
SaludaDev <sistemas@saluda.mx>
2023-03-29 14:50:04 -0600

    d

--- AgendaDeCitas/Modales/AltaEspecialidad.php
+++ AgendaDeCitas/Modales/AltaEspecialidad.php
@@ -41,7 +41,7 @@
               </div>
               <select id="sucursal" class="form-control" name="Sucursal">
                 <option value="">Seleccione una Sucursal:</option>
-                <?
+                <?php
                 $query = $conn->query("SELECT ID_SucursalC,Nombre_Sucursal,ID_H_O_D FROM SucursalesCorre WHERE  ID_H_O_D='" . $row['ID_H_O_D'] . "' AND Nombre_Sucursal !='Matriz'");
                 while ($valores = mysqli_fetch_array($query)) {
                   echo '<option value="' . $valores["ID_SucursalC"] . '">' . $valores["Nombre_Sucursal"] . '</option>';
