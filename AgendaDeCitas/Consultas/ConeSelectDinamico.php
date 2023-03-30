0d4c867c49487f9ff296eeec277559202a306293
SaludaDev <sistemas@saluda.mx>
2023-03-30 14:37:52 -0600

    Update ConeSelectDinamico.php

--- AgendaDeCitas/Consultas/ConeSelectDinamico.php
+++ AgendaDeCitas/Consultas/ConeSelectDinamico.php
@@ -1,3 +1,3 @@
 <?php
-$conn = new mysqli('localhost','u155356178_CorpoSaluda', 'SSalud4Dev2495#$', 'u155356178_DesarrolloSalu') or die(mysqli_error());	
+$conn = new mysqli('localhost','u155356178_CorpoSaluda', 'SSalud4Dev2495#$', 'u155356178_DesarrolloSalu');	
 ?>
