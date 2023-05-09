3fabc9a6ecc3e62ffc288c10db6d9b0eface46f5
SaludaDev <sistemas@saluda.mx>
2023-05-09 10:59:52 -0600

    Update productos.modelo.php

--- POS2/modelos/productos.modelo.php
+++ POS2/modelos/productos.modelo.php
@@ -2,166 +2,166 @@
 
 require_once "conexion.php";
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
+// use PhpOffice\PhpSpreadsheet\IOFactory;
 
 
-class ProductosModelo
-{
+ class ProductosModelo
+ {
 
-    /*===================================================================
-    REALIZAR LA CARGA MASIVA DE PRODUCTOS MEDIANTE ARCHIVO EXCEL
-    ====================================================================*/
-    static public function mdlCargaMasivaProductos($fileProductos)
-    {
+//     /*===================================================================
+//     REALIZAR LA CARGA MASIVA DE PRODUCTOS MEDIANTE ARCHIVO EXCEL
+//     ====================================================================*/
+//     static public function mdlCargaMasivaProductos($fileProductos)
+//     {
 
-        $nombreArchivo = $fileProductos['tmp_name'];
+//         $nombreArchivo = $fileProductos['tmp_name'];
 
-        $documento = IOFactory::load($nombreArchivo);
+//         $documento = IOFactory::load($nombreArchivo);
 
-        $hojaCategorias = $documento->getSheet(1);
-        $numeroFilasCategorias = $hojaCategorias->getHighestDataRow();
+//         $hojaCategorias = $documento->getSheet(1);
+//         $numeroFilasCategorias = $hojaCategorias->getHighestDataRow();
 
-        $hojaProductos = $documento->getSheetByName("Productos");
-        $numeroFilasProductos = $hojaProductos->getHighestDataRow();
+//         $hojaProductos = $documento->getSheetByName("Productos");
+//         $numeroFilasProductos = $hojaProductos->getHighestDataRow();
 
-        $categoriasRegistradas = 0;
-        $productosRegistrados = 0;
+//         $categoriasRegistradas = 0;
+//         $productosRegistrados = 0;
 
-        $stmt = Conexion::conectar()->prepare("delete from venta_detalle");
-        $stmt->execute();
+//         $stmt = Conexion::conectar()->prepare("delete from venta_detalle");
+//         $stmt->execute();
 
-        $stmt = Conexion::conectar()->prepare("delete from venta_cabecera");
-        $stmt->execute();
+//         $stmt = Conexion::conectar()->prepare("delete from venta_cabecera");
+//         $stmt->execute();
 
-        $stmt = Conexion::conectar()->prepare("delete from kardex");
-        $stmt->execute();
+//         $stmt = Conexion::conectar()->prepare("delete from kardex");
+//         $stmt->execute();
 
-        $stmt = Conexion::conectar()->prepare("delete from productos");
-        $stmt->execute();
+//         $stmt = Conexion::conectar()->prepare("delete from productos");
+//         $stmt->execute();
 
-        $stmt = Conexion::conectar()->prepare("delete from categorias");
-        $stmt->execute();
+//         $stmt = Conexion::conectar()->prepare("delete from categorias");
+//         $stmt->execute();
 
-        $stmt = Conexion::conectar()->prepare("delete from productos");
-        $stmt->execute();
+//         $stmt = Conexion::conectar()->prepare("delete from productos");
+//         $stmt->execute();
 
-        //CICLO FOR PARA REGISTROS DE CATEGORIAS
-        for ($i = 2; $i <= $numeroFilasCategorias; $i++) {
+//         //CICLO FOR PARA REGISTROS DE CATEGORIAS
+//         for ($i = 2; $i <= $numeroFilasCategorias; $i++) {
 
-            $categoria = $hojaCategorias->getCellByColumnAndRow(1, $i);
-            $aplica_peso = $hojaCategorias->getCellByColumnAndRow(2, $i);
-            $fecha_actualizacion = date("Y-m-d");
+//             $categoria = $hojaCategorias->getCellByColumnAndRow(1, $i);
+//             $aplica_peso = $hojaCategorias->getCellByColumnAndRow(2, $i);
+//             $fecha_actualizacion = date("Y-m-d");
 
-            if (!empty($categoria)) {
-                $stmt = Conexion::conectar()->prepare("INSERT INTO categorias(nombre_categoria,
-                                                                                aplica_peso,
-                                                                                fecha_actualizacion_categoria)
-                                                                    values(:nombre_categoria,
-                                                                            :aplica_peso,
-                                                                            :fecha_actualizacion_categoria);");
+//             if (!empty($categoria)) {
+//                 $stmt = Conexion::conectar()->prepare("INSERT INTO categorias(nombre_categoria,
+//                                                                                 aplica_peso,
+//                                                                                 fecha_actualizacion_categoria)
+//                                                                     values(:nombre_categoria,
+//                                                                             :aplica_peso,
+//                                                                             :fecha_actualizacion_categoria);");
 
-                $stmt->bindParam(":nombre_categoria", $categoria, PDO::PARAM_STR);
-                $stmt->bindParam(":aplica_peso", $aplica_peso, PDO::PARAM_STR);
-                $stmt->bindParam(":fecha_actualizacion_categoria", $fecha_actualizacion, PDO::PARAM_STR);
+//                 $stmt->bindParam(":nombre_categoria", $categoria, PDO::PARAM_STR);
+//                 $stmt->bindParam(":aplica_peso", $aplica_peso, PDO::PARAM_STR);
+//                 $stmt->bindParam(":fecha_actualizacion_categoria", $fecha_actualizacion, PDO::PARAM_STR);
 
-                if ($stmt->execute()) {
-                    $categoriasRegistradas = $categoriasRegistradas + 1;
-                } else {
-                    $categoriasRegistradas = 0;
-                }
-            }
-        }
+//                 if ($stmt->execute()) {
+//                     $categoriasRegistradas = $categoriasRegistradas + 1;
+//                 } else {
+//                     $categoriasRegistradas = 0;
+//                 }
+//             }
+//         }
 
-        if ($categoriasRegistradas > 0) {
+//         if ($categoriasRegistradas > 0) {
 
-            //CICLO FOR PARA REGISTROS DE PRODUCTOS
-            for ($i = 2; $i <= $numeroFilasProductos; $i++) {
+//             //CICLO FOR PARA REGISTROS DE PRODUCTOS
+//             for ($i = 2; $i <= $numeroFilasProductos; $i++) {
 
-                $codigo_producto = $hojaProductos->getCell("A" . $i);
-                $id_categoria_producto = ProductosModelo::mdlBuscarIdCategoria($hojaProductos->getCell("B" . $i));
-                $descripcion_producto = $hojaProductos->getCell("C" . $i);
-                $precio_compra_producto = $hojaProductos->getCell("D" . $i);
-                $precio_venta_producto = $hojaProductos->getCell("E" . $i)->getCalculatedValue();
-                $precio_mayor_producto = $hojaProductos->getCell("F" . $i)->getCalculatedValue();
-                $precio_oferta_producto = $hojaProductos->getCell("G" . $i)->getCalculatedValue();
-                $stock_producto = $hojaProductos->getCell("H" . $i);
-                $minimo_stock_producto = $hojaProductos->getCell("I" . $i);
-                $ventas_producto = $hojaProductos->getCell("J" . $i);
-                $costo_total_producto = $hojaProductos->getCell("K" . $i);
+//                 $codigo_producto = $hojaProductos->getCell("A" . $i);
+//                 $id_categoria_producto = ProductosModelo::mdlBuscarIdCategoria($hojaProductos->getCell("B" . $i));
+//                 $descripcion_producto = $hojaProductos->getCell("C" . $i);
+//                 $precio_compra_producto = $hojaProductos->getCell("D" . $i);
+//                 $precio_venta_producto = $hojaProductos->getCell("E" . $i)->getCalculatedValue();
+//                 $precio_mayor_producto = $hojaProductos->getCell("F" . $i)->getCalculatedValue();
+//                 $precio_oferta_producto = $hojaProductos->getCell("G" . $i)->getCalculatedValue();
+//                 $stock_producto = $hojaProductos->getCell("H" . $i);
+//                 $minimo_stock_producto = $hojaProductos->getCell("I" . $i);
+//                 $ventas_producto = $hojaProductos->getCell("J" . $i);
+//                 $costo_total_producto = $hojaProductos->getCell("K" . $i);
 
-                if (!empty($codigo_producto) && strlen($codigo_producto) > 0) {
+//                 if (!empty($codigo_producto) && strlen($codigo_producto) > 0) {
 
-                    $stmt = Conexion::conectar()->prepare("INSERT INTO productos(codigo_producto,
-                                                                                id_categoria_producto,
-                                                                                descripcion_producto,
-                                                                                precio_compra_producto,
-                                                                                precio_venta_producto,
-                                                                                precio_mayor_producto,
-                                                                                precio_oferta_producto,
-                                                                                stock_producto,
-                                                                                minimo_stock_producto,
-                                                                                ventas_producto,
-                                                                                costo_total_producto)
-                                                                        values(:codigo_producto,
-                                                                                :id_categoria_producto,
-                                                                                :descripcion_producto,
-                                                                                :precio_compra_producto,
-                                                                                :precio_venta_producto,
-                                                                                :precio_mayor_producto,
-                                                                                :precio_oferta_producto,
-                                                                                :stock_producto,
-                                                                                :minimo_stock_producto,
-                                                                                :ventas_producto,
-                                                                                :costo_total_producto);");
+//                     $stmt = Conexion::conectar()->prepare("INSERT INTO productos(codigo_producto,
+//                                                                                 id_categoria_producto,
+//                                                                                 descripcion_producto,
+//                                                                                 precio_compra_producto,
+//                                                                                 precio_venta_producto,
+//                                                                                 precio_mayor_producto,
+//                                                                                 precio_oferta_producto,
+//                                                                                 stock_producto,
+//                                                                                 minimo_stock_producto,
+//                                                                                 ventas_producto,
+//                                                                                 costo_total_producto)
+//                                                                         values(:codigo_producto,
+//                                                                                 :id_categoria_producto,
+//                                                                                 :descripcion_producto,
+//                                                                                 :precio_compra_producto,
+//                                                                                 :precio_venta_producto,
+//                                                                                 :precio_mayor_producto,
+//                                                                                 :precio_oferta_producto,
+//                                                                                 :stock_producto,
+//                                                                                 :minimo_stock_producto,
+//                                                                                 :ventas_producto,
+//                                                                                 :costo_total_producto);");
 
-                    $stmt->bindParam(":codigo_producto", $codigo_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":id_categoria_producto", $id_categoria_producto[0], PDO::PARAM_STR);
-                    $stmt->bindParam(":descripcion_producto", $descripcion_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":precio_compra_producto", $precio_compra_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":precio_venta_producto", $precio_venta_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":precio_mayor_producto", $precio_mayor_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":precio_oferta_producto", $precio_oferta_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":stock_producto", $stock_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":minimo_stock_producto", $minimo_stock_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":ventas_producto", $ventas_producto, PDO::PARAM_STR);
-                    $stmt->bindParam(":costo_total_producto", $costo_total_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":codigo_producto", $codigo_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":id_categoria_producto", $id_categoria_producto[0], PDO::PARAM_STR);
+//                     $stmt->bindParam(":descripcion_producto", $descripcion_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":precio_compra_producto", $precio_compra_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":precio_venta_producto", $precio_venta_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":precio_mayor_producto", $precio_mayor_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":precio_oferta_producto", $precio_oferta_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":stock_producto", $stock_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":minimo_stock_producto", $minimo_stock_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":ventas_producto", $ventas_producto, PDO::PARAM_STR);
+//                     $stmt->bindParam(":costo_total_producto", $costo_total_producto, PDO::PARAM_STR);
 
-                    if ($stmt->execute()) {
+//                     if ($stmt->execute()) {
 
-                        $productosRegistrados = $productosRegistrados + 1;
+//                         $productosRegistrados = $productosRegistrados + 1;
 
-                        $concepto = 'INVENTARIO INICIAL';
-                        $comprobante = '';
+//                         $concepto = 'INVENTARIO INICIAL';
+//                         $comprobante = '';
 
-                        //REGISTRAMOS KARDEX - INVENTARIO INICIAL
-                        $stmt = Conexion::conectar()->prepare("call prc_registrar_kardex_existencias(:p_codigo_producto,
-                                                                                                    :p_concepto,
-                                                                                                    :p_comprobante,
-                                                                                                    :p_unidades,
-                                                                                                    :p_costo_unitario,
-                                                                                                    :p_costo_total);");
+//                         //REGISTRAMOS KARDEX - INVENTARIO INICIAL
+//                         $stmt = Conexion::conectar()->prepare("call prc_registrar_kardex_existencias(:p_codigo_producto,
+//                                                                                                     :p_concepto,
+//                                                                                                     :p_comprobante,
+//                                                                                                     :p_unidades,
+//                                                                                                     :p_costo_unitario,
+//                                                                                                     :p_costo_total);");
 
-                        $stmt->bindParam(":p_codigo_producto", $codigo_producto, PDO::PARAM_STR);
-                        $stmt->bindParam(":p_concepto", $concepto, PDO::PARAM_STR);
-                        $stmt->bindParam(":p_comprobante", $comprobante, PDO::PARAM_STR);
-                        $stmt->bindParam(":p_unidades", $stock_producto, PDO::PARAM_STR);
-                        $stmt->bindParam(":p_costo_unitario", $precio_compra_producto, PDO::PARAM_STR);
-                        $stmt->bindParam(":p_costo_total", $costo_total_producto, PDO::PARAM_STR);
+//                         $stmt->bindParam(":p_codigo_producto", $codigo_producto, PDO::PARAM_STR);
+//                         $stmt->bindParam(":p_concepto", $concepto, PDO::PARAM_STR);
+//                         $stmt->bindParam(":p_comprobante", $comprobante, PDO::PARAM_STR);
+//                         $stmt->bindParam(":p_unidades", $stock_producto, PDO::PARAM_STR);
+//                         $stmt->bindParam(":p_costo_unitario", $precio_compra_producto, PDO::PARAM_STR);
+//                         $stmt->bindParam(":p_costo_total", $costo_total_producto, PDO::PARAM_STR);
 
-                        $stmt->execute();
-                    } else {
-                        $productosRegistrados = 0;
-                    }
-                }
-            }
-        }
+//                         $stmt->execute();
+//                     } else {
+//                         $productosRegistrados = 0;
+//                     }
+//                 }
+//             }
+//         }
 
 
-        $respuesta["totalCategorias"] = $categoriasRegistradas;
-        $respuesta["totalProductos"] = $productosRegistrados;
+//         $respuesta["totalCategorias"] = $categoriasRegistradas;
+//         $respuesta["totalProductos"] = $productosRegistrados;
 
-        return $respuesta;
-    }
+//         return $respuesta;
+//     }
 
     /*===================================================================
     BUSCAR EL ID DE UNA CATEGORIA POR EL NOMBRE DE LA CATEGORIA
