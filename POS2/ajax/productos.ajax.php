9916c6d8048a545f51a29338b25dfaf8aa28d582
SaludaDev <sistemas@saluda.mx>
2023-05-09 16:00:15 -0600

    Update productos.ajax.php

--- POS2/ajax/productos.ajax.php
+++ POS2/ajax/productos.ajax.php
@@ -1,15 +1,35 @@
 <?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "u155356178_CorpoSaluda";
+$password = "SSalud4Dev2495#$";
+$dbname = "u155356178_DesarrolloSalu";
 
-// Verificar si se recibió la acción adecuada
-if (isset($_POST['accion']) && $_POST['accion'] == 6) {
-
-    // Aquí iría tu lógica para obtener los productos y devolverlos en formato JSON
-    $productos = array(
-        array('descripcion_producto' => 'Producto 1'),
-        array('descripcion_producto' => 'Producto 2'),
-        array('descripcion_producto' => 'Producto 3')
-    );
-
-    // Devolver los datos en formato JSON
-    echo json_encode($productos);
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Verificar que se recibió la acción
+if(isset($_POST['accion'])) {
+
+    // Obtener los productos desde la base de datos
+    if($_POST['accion'] == 6) {
+        $items = array();
+
+        $sql = "SELECT descripcion_producto FROM productos";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                array_push($items, $row['descripcion_producto']);
+            }
+        }
+
+        echo json_encode($items);
+    }
+
+}
+
+$conn->close();
+?>
