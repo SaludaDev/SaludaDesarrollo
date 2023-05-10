e97d6b2ea795fb5d7ef20f18b1f6130659803f0e
SaludaDev <sistemas@saluda.mx>
2023-05-10 11:33:46 -0600

    F

--- POS2/RealizarVenta.php
+++ POS2/RealizarVenta.php
@@ -466,39 +466,37 @@ $(document).ready(function() {
     /* ======================================================================================
 TRAER LISTADO DE PRODUCTOS PARA INPUT DE AUTOCOMPLETADO
 ======================================================================================*/
-    // $.ajax({
-    //     async: false,
-    //      url: "ajax/productos.ajax.php",
-    //     method: "POST",
-    //     data: {
-    //         'accion': 6
-    //     },
-    //     dataType: 'json',
-    //     success: function(respuesta) {
+$.ajax({
+        async: false,
+         url: "ajax/productos.ajax.php",
+        method: "POST",
+        data: {
+            'accion': 6
+        },
+        dataType: 'json',
+        success: function(respuesta) {
 
-    //         for (let i = 0; i < respuesta.length; i++) {
-    //             items.push(respuesta[i]['descripcion_producto'])
-    //         }
+            for (let i = 0; i < respuesta.length; i++) {
+                items.push(respuesta[i]['descripcion_producto'])
+            }
 
-    //         $("#iptCodigoVenta").autocomplete({
+            $("#iptCodigoVenta").autocomplete({
 
-    //             source: items,
-    //             select: function(event, ui) {
+                source: items,
+                select: function(event, ui) {
 
-    //                 CargarProductos(ui.item.value);
+                    CargarProductos(ui.item.value);
 
-    //                 $("#iptCodigoVenta").val("");
+                    $("#iptCodigoVenta").val("");
 
-    //                 $("#iptCodigoVenta").focus();
+                    $("#iptCodigoVenta").focus();
 
-    //                 return false;
-    //             }
-    //         })
+                    return false;
+                }
+            })
 
-    //     }
-    // });
-
-
+        }
+    });
     /* ======================================================================================
     EVENTO QUE REGISTRA EL PRODUCTO EN EL LISTADO CUANDO SE INGRESA EL CODIGO DE BARRAS
     ======================================================================================*/
@@ -555,6 +553,161 @@ $.ajax({
 });
 
 
+/*===================================================================*/
+//FUNCION PARA CARGAR PRODUCTOS EN EL DATATABLE
+/*===================================================================*/
+function CargarProductos(producto = "") {
+
+if (producto != "") {
+    var codigo_producto = producto;
+
+} else {
+    var codigo_producto = $("#iptCodigoVenta").val();
+}
+
+codigo_producto = $.trim(codigo_producto.split('/')[0]);
+// console.log("🚀 ~ file: ventas.php:844 ~ CargarProductos ~ codigo_producto", codigo_producto)
+
+// return;
+
+var producto_repetido = 0;
+
+/*===================================================================*/
+// AUMENTAMOS LA CANTIDAD SI EL PRODUCTO YA EXISTE EN EL LISTADO
+/*===================================================================*/
+table.rows().eq(0).each(function(index) {
+
+    var row = table.row(index);
+    var data = row.data();
+    console.log("🚀 ~ file: ventas.php:829 ~ table.rows ~ data", $.parseHTML(data['cantidad'])[0]['value'])
+
+    if (codigo_producto == data['codigo_producto']) {
+
+        producto_repetido = 1;
+
+        cantidad_a_comprar = parseFloat($.parseHTML(data['cantidad'])[0]['value']) + 1;
+
+        $.ajax({
+            async: false,
+            url: "ajax/productos.ajax.php",
+            method: "POST",
+            data: {
+                'accion': 8,
+                'codigo_producto': codigo_producto,
+                'cantidad_a_comprar': cantidad_a_comprar
+            },
+            dataType: 'json',
+            success: function(respuesta) {
+
+                if (parseInt(respuesta['existe']) == 0) {
+
+                    mensajeToast('error', ' El producto ' + data['descripcion_producto'] + ' ya no tiene stock');
+
+                    $("#iptCodigoVenta").val("");
+                    $("#iptCodigoVenta").focus();
+
+                } else {
+
+                    // AUMENTAR EN 1 EL VALOR DE LA CANTIDAD
+                    table.cell(index, 5).data('<input type="text" style="width:80px;" codigoProducto = "' + codigo_producto + '" class="form-control text-center iptCantidad m-0 p-0" value="' + cantidad_a_comprar + '">').draw();
+
+
+                    // ACTUALIZAR EL NUEVO PRECIO DEL ITEM DEL LISTADO DE VENTA
+                    NuevoPrecio = (parseFloat(cantidad_a_comprar) * data['precio_venta_producto'].replaceAll("MXN ", "")).toFixed(2);
+                    NuevoPrecio = "MXN " + NuevoPrecio;
+                    table.cell(index, 7).data(NuevoPrecio).draw();
+
+                    // RECALCULAMOS TOTALES
+                    recalcularTotales();
+                }
+            }
+        });
+
+    }
+});
+
+// return;
+
+if (producto_repetido == 1) {
+    return;
+}
+
+console.log(codigo_producto);
+
+$.ajax({
+    url: "ajax/productos.ajax.php",
+    method: "POST",
+    data: {
+        'accion': 7, //BUSCAR PRODUCTOS POR SU CODIGO DE BARRAS
+        'codigo_producto': codigo_producto
+    },
+    dataType: 'json',
+    success: function(respuesta) {
+
+        console.log(respuesta);
+        /*===================================================================*/
+        //SI LA RESPUESTA ES VERDADERO, TRAE ALGUN DATO
+        /*===================================================================*/
+        if (respuesta) {
+
+            var TotalVenta = 0.00;
+
+            table.row.add({
+                'id': itemProducto,
+                'codigo_producto': respuesta['codigo_producto'],
+                'id_categoria': respuesta['id_categoria'],
+                'nombre_categoria': respuesta['nombre_categoria'],
+                'descripcion_producto': respuesta['descripcion_producto'],
+                'cantidad': '<input type="text" style="width:80px;" codigoProducto = "' + respuesta['codigo_producto'] + '" class="form-control text-center iptCantidad p-0 m-0" value="1">',
+                'precio_venta_producto': respuesta['precio_venta_producto'],
+                'total': respuesta['total'],
+                'acciones': "<center>" +
+                    // "<span class='btnAumentarCantidad text-success px-1' style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Aumentar Stock'> " +
+                    // "<i class='fas fa-cart-plus fs-5'></i> " +
+                    // "</span> " +
+                    // "<span class='btnDisminuirCantidad text-warning px-1' style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Disminuir Stock'> " +
+                    // "<i class='fas fa-cart-arrow-down fs-5'></i> " +
+                    // "</span> " +
+                    "<span class='btnEliminarproducto text-danger px-1'style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Eliminar producto'> " +
+                    "<i class='fas fa-trash fs-5'> </i> " +
+                    "</span>" +
+                    "<div class='btn-group'>" +
+                    "<button type='button' class=' p-0 btn btn-primary transparentbar dropdown-toggle btn-sm' data-bs-toggle='dropdown' aria-expanded='false'>" +
+                    "<i class='fas fa-cog text-primary fs-5'></i> <i class='fas fa-chevron-down text-primary'></i>" +
+                    "</button>" +
+
+                    "<ul class='dropdown-menu'>" +
+                    "<li><a class='dropdown-item' codigo = '" + respuesta['codigo_producto'] + "' precio=' " + respuesta['precio_venta_producto'] + "' style='cursor:pointer; font-size:14px;'>Normal (" + respuesta['precio_venta_producto'] + ")</a></li>" +
+                    "<li><a class='dropdown-item' codigo = '" + respuesta['codigo_producto'] + "' precio=' " + respuesta['precio_mayor_producto'] + "' style='cursor:pointer; font-size:14px;'>Por Mayor (MXN " + parseFloat(respuesta['precio_mayor_producto']).toFixed(2) + ")</a></li>" +
+                    "<li><a class='dropdown-item' codigo = '" + respuesta['codigo_producto'] + "' precio=' " + respuesta['precio_oferta_producto'] + "' style='cursor:pointer; font-size:14px;'>Oferta (MXN " + parseFloat(respuesta['precio_oferta_producto']).toFixed(2) + ")</a></li>" +
+                    "</ul>" +
+                    "</div>" +
+                    "</center>",
+                'aplica_peso': respuesta['aplica_peso'],
+                'precio_mayor_producto': respuesta['precio_mayor_producto'],
+                'precio_oferta_producto': respuesta['precio_oferta_producto']
+            }).draw();
+
+            itemProducto = itemProducto + 1;
+
+            //  Recalculamos el total de la venta
+            recalcularTotales();
+
+            /*===================================================================*/
+            //SI LA RESPUESTA ES FALSO, NO TRAE ALGUN DATO
+            /*===================================================================*/
+        } else {
+
+            mensajeToast('error', 'EL PRODUCTO NO EXISTE O NO TIENE STOCK');
+
+            $("#iptCodigoVenta").val("");
+            $("#iptCodigoVenta").focus();
+        }
+
+    }
+});
+
+}
     /* ======================================================================================
     EVENTO PARA DESMINUIR LA CANTIDAD DE UN PRODUCTO DEL LISTADO
     ======================================================================================*/
@@ -878,162 +1031,7 @@ function recalcularTotales() {
     $("#iptCodigoVenta").focus();
 }
 
-
-/*===================================================================*/
-//FUNCION PARA CARGAR PRODUCTOS EN EL DATATABLE
-/*===================================================================*/
-function CargarProductos(producto = "") {
-
-    if (producto != "") {
-        var codigo_producto = producto;
-
-    } else {
-        var codigo_producto = $("#iptCodigoVenta").val();
-    }
-
-    codigo_producto = $.trim(codigo_producto.split('/')[0]);
-    // console.log("🚀 ~ file: ventas.php:844 ~ CargarProductos ~ codigo_producto", codigo_producto)
-
-    // return;
-
-    var producto_repetido = 0;
-
-    /*===================================================================*/
-    // AUMENTAMOS LA CANTIDAD SI EL PRODUCTO YA EXISTE EN EL LISTADO
-    /*===================================================================*/
-    table.rows().eq(0).each(function(index) {
-
-        var row = table.row(index);
-        var data = row.data();
-        console.log("🚀 ~ file: ventas.php:829 ~ table.rows ~ data", $.parseHTML(data['cantidad'])[0]['value'])
-
-        if (codigo_producto == data['codigo_producto']) {
-
-            producto_repetido = 1;
-
-            cantidad_a_comprar = parseFloat($.parseHTML(data['cantidad'])[0]['value']) + 1;
-
-            $.ajax({
-                async: false,
-                url: "ajax/productos.ajax.php",
-                method: "POST",
-                data: {
-                    'accion': 8,
-                    'codigo_producto': codigo_producto,
-                    'cantidad_a_comprar': cantidad_a_comprar
-                },
-                dataType: 'json',
-                success: function(respuesta) {
-
-                    if (parseInt(respuesta['existe']) == 0) {
-
-                        mensajeToast('error', ' El producto ' + data['descripcion_producto'] + ' ya no tiene stock');
-
-                        $("#iptCodigoVenta").val("");
-                        $("#iptCodigoVenta").focus();
-
-                    } else {
-
-                        // AUMENTAR EN 1 EL VALOR DE LA CANTIDAD
-                        table.cell(index, 5).data('<input type="text" style="width:80px;" codigoProducto = "' + codigo_producto + '" class="form-control text-center iptCantidad m-0 p-0" value="' + cantidad_a_comprar + '">').draw();
-
-
-                        // ACTUALIZAR EL NUEVO PRECIO DEL ITEM DEL LISTADO DE VENTA
-                        NuevoPrecio = (parseFloat(cantidad_a_comprar) * data['precio_venta_producto'].replaceAll("MXN ", "")).toFixed(2);
-                        NuevoPrecio = "MXN " + NuevoPrecio;
-                        table.cell(index, 7).data(NuevoPrecio).draw();
-
-                        // RECALCULAMOS TOTALES
-                        recalcularTotales();
-                    }
-                }
-            });
-
-        }
-    });
-
-    // return;
-
-    if (producto_repetido == 1) {
-        return;
-    }
-
-    console.log(codigo_producto);
-
-    $.ajax({
-        url: "ajax/productos.ajax.php",
-        method: "POST",
-        data: {
-            'accion': 7, //BUSCAR PRODUCTOS POR SU CODIGO DE BARRAS
-            'codigo_producto': codigo_producto
-        },
-        dataType: 'json',
-        success: function(respuesta) {
-
-            console.log(respuesta);
-            /*===================================================================*/
-            //SI LA RESPUESTA ES VERDADERO, TRAE ALGUN DATO
-            /*===================================================================*/
-            if (respuesta) {
-
-                var TotalVenta = 0.00;
-
-                table.row.add({
-                    'id': itemProducto,
-                    'codigo_producto': respuesta['codigo_producto'],
-                    'id_categoria': respuesta['id_categoria'],
-                    'nombre_categoria': respuesta['nombre_categoria'],
-                    'descripcion_producto': respuesta['descripcion_producto'],
-                    'cantidad': '<input type="text" style="width:80px;" codigoProducto = "' + respuesta['codigo_producto'] + '" class="form-control text-center iptCantidad p-0 m-0" value="1">',
-                    'precio_venta_producto': respuesta['precio_venta_producto'],
-                    'total': respuesta['total'],
-                    'acciones': "<center>" +
-                        // "<span class='btnAumentarCantidad text-success px-1' style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Aumentar Stock'> " +
-                        // "<i class='fas fa-cart-plus fs-5'></i> " +
-                        // "</span> " +
-                        // "<span class='btnDisminuirCantidad text-warning px-1' style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Disminuir Stock'> " +
-                        // "<i class='fas fa-cart-arrow-down fs-5'></i> " +
-                        // "</span> " +
-                        "<span class='btnEliminarproducto text-danger px-1'style='cursor:pointer;' data-bs-toggle='tooltip' data-bs-placement='top' title='Eliminar producto'> " +
-                        "<i class='fas fa-trash fs-5'> </i> " +
-                        "</span>" +
-                        "<div class='btn-group'>" +
-                        "<button type='button' class=' p-0 btn btn-primary transparentbar dropdown-toggle btn-sm' data-bs-toggle='dropdown' aria-expanded='false'>" +
-                        "<i class='fas fa-cog text-primary fs-5'></i> <i class='fas fa-chevron-down text-primary'></i>" +
-                        "</button>" +
-
-                        "<ul class='dropdown-menu'>" +
-                        "<li><a class='dropdown-item' codigo = '" + respuesta['codigo_producto'] + "' precio=' " + respuesta['precio_venta_producto'] + "' style='cursor:pointer; font-size:14px;'>Normal (" + respuesta['precio_venta_producto'] + ")</a></li>" +
-                        "<li><a class='dropdown-item' codigo = '" + respuesta['codigo_producto'] + "' precio=' " + respuesta['precio_mayor_producto'] + "' style='cursor:pointer; font-size:14px;'>Por Mayor (MXN " + parseFloat(respuesta['precio_mayor_producto']).toFixed(2) + ")</a></li>" +
-                        "<li><a class='dropdown-item' codigo = '" + respuesta['codigo_producto'] + "' precio=' " + respuesta['precio_oferta_producto'] + "' style='cursor:pointer; font-size:14px;'>Oferta (MXN " + parseFloat(respuesta['precio_oferta_producto']).toFixed(2) + ")</a></li>" +
-                        "</ul>" +
-                        "</div>" +
-                        "</center>",
-                    'aplica_peso': respuesta['aplica_peso'],
-                    'precio_mayor_producto': respuesta['precio_mayor_producto'],
-                    'precio_oferta_producto': respuesta['precio_oferta_producto']
-                }).draw();
-
-                itemProducto = itemProducto + 1;
-
-                //  Recalculamos el total de la venta
-                recalcularTotales();
-
-                /*===================================================================*/
-                //SI LA RESPUESTA ES FALSO, NO TRAE ALGUN DATO
-                /*===================================================================*/
-            } else {
-
-                mensajeToast('error', 'EL PRODUCTO NO EXISTE O NO TIENE STOCK');
-
-                $("#iptCodigoVenta").val("");
-                $("#iptCodigoVenta").focus();
-            }
-
-        }
-    });
-
-} /* FIN CargarProductos */
+ /* FIN CargarProductos */
 
 /*===================================================================*/
 //REALIZAR LA VENTA
