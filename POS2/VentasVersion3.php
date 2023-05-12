cb42b831dc53e877b1533f0ef3d57b988ac66c2c
SaludaDev <sistemas@saluda.mx>
2023-05-12 10:40:50 -0600

    Update VentasVersion3.php

--- POS2/VentasVersion3.php
+++ POS2/VentasVersion3.php
@@ -111,6 +111,28 @@ function buscarArticulo(){
 		}
 	});
 }
+
+function agregarArticulo(articulo){	
+	if ($('#detIdModal' + articulo.id).length) { // si ya esta agregado advierto
+		msjError('El artículo ya se encuentra incluido');
+	} else { // si es nuevo agrego
+		var tr = ''; 
+		var btnEliminar = '<button type="button" class="btn btn-xs btn-danger" onclick="$(this).parent().parent().remove();"><i class="glyphicon glyphicon-minus"></i></button>';
+		var inputId = '<input type="hidden" name="detIdModal[' + articulo.id + ']" value="' + articulo.id + '" />';
+		var inputCantidad = '<input type="hidden" name="detCantidadModal[' + articulo.id + ']" value="' + articulo.cantidad + '" />';
+		
+		tr += '<tr>';
+			tr += '<td>' + articulo.descripcion + '</td>';
+			tr += '<td>' + articulo.cantidad + '</td>';
+			tr += '<td>' + btnEliminar + inputId + inputCantidad + '</td>';
+		tr += '</tr>';
+		
+		$('#tablaAgregarArticulos tbody').append(tr);
+	}
+	
+	$('#codigoEscaneado').val('');
+	$('#codigoEscaneado').focus();
+}
 </script>
      <!-- /.content-wrapper -->
    
