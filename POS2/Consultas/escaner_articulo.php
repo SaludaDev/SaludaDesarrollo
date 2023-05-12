8103613c6e0d9eb6a373a1bfdf77fadca2260ddc
SaludaDev <sistemas@saluda.mx>
2023-05-12 11:01:37 -0600

    Update escaner_articulo.php

--- POS2/Consultas/escaner_articulo.php
+++ POS2/Consultas/escaner_articulo.php
@@ -1,27 +1,36 @@
-<?php
+<?php 
+// configuración de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u155356178_CorpoSaluda');
+define('DB_PASS', 'SSalud4Dev2495#$');
+define('DB_NAME', 'u155356178_DesarrolloSalu');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $codigo = $_POST['codigoEscaneado'];
+// conexión a la base de datos
+$conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    $conexion = new mysqli("localhost", "u155356178_CorpoSaluda", "SSalud4Dev2495#$","u155356178_DesarrolloSalu");
-
-    if ($conexion->connect_error) {
-        die('Error de conexión a la base de datos: ' . $conexion->connect_error);
-    }
-
-   
-    $stmt = $conexion->prepare('SELECT * FROM productos WHERE codigo_producto = ?');
-    $stmt->bind_param('s', $codigo);
-
-    if (!$stmt->execute()) {
-        die('Error al ejecutar la consulta: ' . $stmt->error);
-    }
-
-    $resultado = $stmt->get_result();
-    $data = $resultado->fetch_assoc();
-    echo json_encode($data);
-
-    $stmt->close();
-    $conexion->close();
+// verificar si hay errores de conexión
+if ($conexion->connect_error) {
+    die('Error de conexión a la base de datos: ' . $conexion->connect_error);
 }
+
+// preparar la consulta
+$codigo = $_POST['codigoEscaneado'];
+$stmt = $conexion->prepare('SELECT * FROM articulos WHERE codigo = ?');
+$stmt->bind_param('s', $codigo);
+
+// ejecutar la consulta
+if (!$stmt->execute()) {
+    die('Error al ejecutar la consulta: ' . $stmt->error);
+}
+
+// obtener el resultado
+$resultado = $stmt->get_result();
+$data = $resultado->fetch_assoc();
+
+// cerrar la consulta y la conexión
+$stmt->close();
+$conexion->close();
+
+// mostrar el resultado en formato JSON
+echo json_encode($data);
 ?>
