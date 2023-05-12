f7815e01e747c3f6b16f9b7c81938e302b60020a
SaludaDev <sistemas@saluda.mx>
2023-05-12 12:58:20 -0600

    Update VentasVersion3.php

--- POS2/VentasVersion3.php
+++ POS2/VentasVersion3.php
@@ -41,7 +41,44 @@ include ("db_connection.php");
 </script>
 
 <button class="btn btn-success" type="button" id="btnNuevo" data-toggle="modal" data-target="#modalArticulos" data-keyboard="false" data-backdrop="static"><i class="fa fa-plus"></i> Nuevo Bulto</button>
-
+<!-- Modal -->
+<div class="modal fade" id="modalArticulos" tabindex="-1" role="dialog" aria-labelledby="modalArticulosLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="modalArticulosLabel">Ingreso de Artículos</h4>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<label>Escanear Código de Barras</label>
+					<div class="input-group">
+						<div class="input-group-addon"><i class="fa fa-barcode"></i></div>
+						<input type="text" class="form-control producto" name="codigoEscaneado" id="codigoEscaneado" autocomplete="off" onchange="buscarArticulo();">
+					</div>
+				</div>
+				<div>
+					<table class="table table-striped" id="tablaAgregarArticulos">
+						<thead>	
+							<tr>
+								<th>Producto</th>
+								<th>Cantidad</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+						
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal" id="btnCerrarModal">Cerrar</button>
+				<button type="button" class="btn btn-primary" id="btnAgregar" onclick="agregar();">Agregar</button>
+			</div>
+		</div>
+	</div>
+</div>
 <!-- FINALIZA DATA DE AGENDA -->
       </div>
       </div>
