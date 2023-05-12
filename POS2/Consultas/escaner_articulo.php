77db62b77822433e1ceb8427d6b8bea45a66403a
SaludaDev <sistemas@saluda.mx>
2023-05-12 12:56:15 -0600

    Update escaner_articulo.php

--- POS2/Consultas/escaner_articulo.php
+++ POS2/Consultas/escaner_articulo.php
@@ -1,42 +1,41 @@
 <?php
-    $servername = "localhost";
-    $username = "u155356178_CorpoSaluda";
-    $password = "SSalud4Dev2495#$";
-    $dbname = "u155356178_DesarrolloSalu";
- 
-    
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Verificar la conexión
-    if ($conn->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
-    }
-    
-    // Obtener el código de barras enviado por AJAX
-    $codigo = $_POST['codigoEscaneado'];
-    
-    // Consultar la base de datos para obtener el artículo correspondiente al código de barras
-    $sql = "SELECT * FROM articulos WHERE codigo = '$codigo'";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-        // Si se encontró el artículo, devolver sus datos en formato JSON
-        $row = $result->fetch_assoc();
-        $data = array(
-            "id" => $row["id"],
-            "descripcion" => $row["descripcion"],
-            "cantidad" => $row["cantidad"],
-            "codigo" => $row["codigo"]
-        );
-        echo json_encode($data);
-    } else {
-        // Si no se encontró el artículo, devolver un array vacío en formato JSON
-        $data = array();
-        echo json_encode($data);
-    }
-    
-    // Cerrar la conexión a la base de datos
-    $conn->close();
-    
-    ?>
-    
+   
+$dbHost = 'localhost';
+$dbUser = 'u155356178_CorpoSaluda';
+$dbPass = 'SSalud4Dev2495#$';
+$dbName = 'u155356178_DesarrolloSalu';
+
+$db = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+if ($db->connect_errno) {
+    echo "Error en la conexión a la base de datos: " . $db->connect_error;
+    exit();
+}
+
+// Obtención del código de barras enviado por AJAX
+$codigoEscaneado = isset($_POST['codigoEscaneado']) ? trim($_POST['codigoEscaneado']) : '';
+
+if (empty($codigoEscaneado)) {
+    echo json_encode(array('error' => 'Código de barras vacío'));
+    exit();
+}
+
+// Consulta a la base de datos para obtener los datos del artículo
+$query = "SELECT id, descripcion, cantidad FROM articulos WHERE codigo_barras = ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param('s', $codigoEscaneado);
+$stmt->execute();
+
+$resultado = $stmt->get_result();
+
+if ($resultado->num_rows === 0) {
+    echo json_encode(array('error' => 'No se encontró ningún artículo con el código de barras especificado'));
+    exit();
+}
+
+// Si se encontró el artículo, se devuelve su información en formato JSON
+$articulo = $resultado->fetch_assoc();
+echo json_encode($articulo);
+
+$stmt->close();
+$db->close();
