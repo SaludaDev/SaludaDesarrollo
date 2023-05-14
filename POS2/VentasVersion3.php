df725c1c64801a949b14611ce4d9dbb9a85341ac
SaludaDev <sistemas@saluda.mx>
2023-05-13 20:18:54 -0600

    Update VentasVersion3.php

--- POS2/VentasVersion3.php
+++ POS2/VentasVersion3.php
@@ -46,62 +46,80 @@ include ("db_connection.php");
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-		<!-- Código HTML -->
-
-<div class="row">
-  <div class="col-md-12">
-    <div class="form-group">
-      <label for="codigoEscaneado">Código de barras</label>
-      <input type="text" id="codigoEscaneado" name="codigoEscaneado" class="form-control" autofocus>
-    </div>
-    <button type="button" class="btn btn-primary" onclick="buscarArticulo()">Buscar</button>
-  </div>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="modalArticulosLabel">Ingreso de Artículos</h4>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<label>Escanear Código de Barras</label>
+					<div class="input-group">
+						<div class="input-group-addon"><i class="fa fa-barcode"></i></div>
+						<input type="text" class="form-control producto" name="codigoEscaneado" id="codigoEscaneado" autocomplete="off" onchange="buscarArticulo();">
+					</div>
+				</div>
+				<div>
+					<table class="table table-striped" id="tablaAgregarArticulos">
+						<thead>	
+							<tr>
+								<th>Producto</th>
+								<th>Cantidad</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+						
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal" id="btnCerrarModal">Cerrar</button>
+				<button type="button" class="btn btn-primary" id="btnAgregar" onclick="agregarArticulo();">Agregar</button>
+			</div>
+		</div>
+	</div>
 </div>
-
-<div class="row">
-  <div class="col-md-12">
-    <table class="table table-striped" id="tablaAgregarArticulos">
-      <thead>
-        <tr>
-          <th>Descripción</th>
-          <th>Cantidad</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
-    </table>
-  </div>
-</div>
-
+<!-- FINALIZA DATA DE AGENDA -->
+      </div>
+      </div>
+      </div>
+      </div>
+   
 <script>
-  function buscarArticulo() {
-    var codigoEscaneado = $('#codigoEscaneado').val();
-    var formData = new FormData();
-    formData.append('codigoEscaneado', codigoEscaneado);
 
-    $.ajax({
-      url: "Consultas/escaner_articulo.php",
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      dataType: 'json',
-      success: function(data) {
-        if (data.length === 0) {
-          msjError('No encontrado');
-        } else if (data.codigo) {
-          msj('Artículo encontrado');
-          agregarArticulo(data);
-        }
-      },
-      error: function(data) {
-        msjError('Se ha producido un error al intentar buscar el Artículo.');
-      }
-    });
-  }
 
-  function agregarArticulo(articulo){	
+function buscarArticulo(){	
+	var codigoEscaneado = $('#codigoEscaneado').val();
+	var formData = new FormData();
+	formData.append('codigoEscaneado', codigoEscaneado);
+	
+	$.ajax({
+		url: "Consultas/escaner_articulo.php",
+		type: 'POST',
+		data: formData,
+		processData: false,
+		contentType: false,
+		dataType: 'json',
+		success: function(data) {
+			if (data.length === 0) {
+				msjError('No Encontrado');
+				
+				$('#codigoEscaneado').val('');
+				$('#codigoEscaneado').focus();
+			} else if (data.codigo) {
+				msj('Artículo encontrado');
+				
+				agregarArticulo(data);
+			}
+		},
+		error: function(data) {
+			msjError('Se ha producido un error al intentar buscar el Artículo.');
+		}
+	});
+}
+
+
+function agregarArticulo(articulo){	
 	if ($('#detIdModal' + articulo.id).length) { // si ya esta agregado advierto
 		msjError('El artículo ya se encuentra incluido');
 	} else { // si es nuevo agrego
@@ -116,18 +134,13 @@ include ("db_connection.php");
 			tr += '<td>' + btnEliminar + inputId + inputCantidad + '</td>';
 		tr += '</tr>';
 		
-		// Mostrar la tabla
-		var tabla = document.getElementById("tablaAgregarArticulos");
-		var tbody = tabla.getElementsByTagName("tbody")[0];
-		tbody.innerHTML += tr;
+		$('#tablaAgregarArticulos tbody').append(tr);
 	}
 	
 	$('#codigoEscaneado').val('');
 	$('#codigoEscaneado').focus();
 }
-
 </script>
-
      <!-- /.content-wrapper -->
    
      <!-- Control Sidebar -->
