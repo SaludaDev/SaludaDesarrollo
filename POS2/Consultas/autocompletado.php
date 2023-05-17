e09131becef7223a052b172b83beec234e2d19fc
SaludaDev <sistemas@saluda.mx>
2023-05-17 16:40:38 -0600

    f

--- POS2/Consultas/autocompletado.php
+++ POS2/Consultas/autocompletado.php
@@ -11,25 +11,23 @@ if (!$conn) {
   die('Error de conexión: ' . mysqli_connect_error());
 }
 
-// Obtén el término de búsqueda enviado desde el campo de autocompletado
 $term = $_GET['term'];
 
-// Realiza la consulta utilizando el término de búsqueda
-$query = "SELECT Cod_Barra, Nombre_Prod FROM Stock_POS WHERE  Cod_Barra LIKE '%{$term}%' OR Nombre_Prod LIKE '%{$term}%' AND Fk_sucursal ='".$row['Fk_sucursal']."'";
+// Asigna el valor de Fk_sucursal antes de la consulta
+$sucursal =21;
+
+$query = "SELECT Cod_Barra, Nombre_Prod FROM Stock_POS WHERE  Cod_Barra LIKE '%{$term}%' OR Nombre_Prod LIKE '%{$term}%' AND Fk_sucursal ='".$sucursal."'";
 $result = mysqli_query($conn, $query);
 
-// Genera un array con los resultados de autocompletado
 $autocompletado = array();
 while ($row = mysqli_fetch_assoc($result)) {
   $autocompletado[] = array(
-    'label' => $row['Cod_Barra'] . ' - ' . $row['Nombre_Prod'], // Texto que se muestra en el autocompletado
-    'value' => $row['Cod_Barra'] // Valor que se selecciona al elegir un resultado del autocompletado
+    'label' => $row['Cod_Barra'] . ' - ' . $row['Nombre_Prod'],
+    'value' => $row['Cod_Barra']
   );
 }
 
-// Devuelve los resultados de autocompletado como JSON
 echo json_encode($autocompletado);
 
-// Cierra la conexión a la base de datos
 mysqli_close($conn);
 ?>
