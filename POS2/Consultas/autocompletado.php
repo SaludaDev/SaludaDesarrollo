57e2ae200e6e65697212fbd887fc61869d90bfef
SaludaDev <sistemas@saluda.mx>
2023-05-18 14:05:57 -0600

    Update autocompletado.php

--- POS2/Consultas/autocompletado.php
+++ POS2/Consultas/autocompletado.php
@@ -1,5 +1,4 @@
 <?php
- include "Consultas.php";
 // Conecta con la base de datos y realiza la consulta para obtener los resultados de autocompletado
 $dbHost = 'localhost'; // Cambia esto por la dirección de tu servidor de base de datos
 $dbUsername = 'u155356178_CorpoSaluda'; // Cambia esto por tu nombre de usuario de la base de datos
@@ -15,21 +14,22 @@ if (!$conn) {
 // Obtén el término de búsqueda enviado desde el campo de autocompletado
 $term = $_GET['term'];
 
-$fk_sucursal = $row['Fk_sucursal']; // Cambia esto por el valor deseado
-
 // Realiza la consulta utilizando el término de búsqueda
-$query = "SELECT Cod_Barra, Nombre_Prod FROM Stock_POS WHERE (Cod_Barra LIKE '%{$term}%' OR Nombre_Prod LIKE '%{$term}%') AND Fk_sucursal ='".$fk_sucursal."'";
+$query = "SELECT Cod_Barra, Nombre_Prod FROM Stock_POS WHERE Cod_Barra LIKE '%{$term}%' OR Nombre_Prod LIKE '%{$term}%'";
 $result = mysqli_query($conn, $query);
 
+// Genera un array con los resultados de autocompletado
 $autocompletado = array();
 while ($row = mysqli_fetch_assoc($result)) {
   $autocompletado[] = array(
-    'label' => $row['Cod_Barra'] . ' - ' . $row['Nombre_Prod'],
-    'value' => $row['Cod_Barra']
+    'label' => $row['Cod_Barra'] . ' - ' . $row['Nombre_Prod'], // Texto que se muestra en el autocompletado
+    'value' => $row['Cod_Barra'] // Valor que se selecciona al elegir un resultado del autocompletado
   );
 }
 
+// Devuelve los resultados de autocompletado como JSON
 echo json_encode($autocompletado);
 
+// Cierra la conexión a la base de datos
 mysqli_close($conn);
 ?>
