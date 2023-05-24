53dfc6e5005096f895e75b1d90965ec7acb7702a
SaludaDev <sistemas@saluda.mx>
2023-05-24 14:33:08 -0600

    Update RealizarVenta.php

--- POS2/RealizarVenta.php
+++ POS2/RealizarVenta.php
@@ -381,7 +381,10 @@ Aqui va el corte de caja
 </div>
 
 </div>
-
+<!-- SCRIPTS -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
 <script>
   $(document).ready(function () {
         $('#tablaAgregarArticulos').DataTable({
